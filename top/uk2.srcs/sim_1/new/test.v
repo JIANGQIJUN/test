9e74d5b29bf0d8e3374512074970a0ab4315c89f
jiang <923684862@qq.com>
2022-04-11 04:01:06 +0800

    添加两个探针，用于检测内插前的信道系数。内插后的信道系数仍有问题。

--- top/uk2.srcs/sim_1/new/test.v
+++ top/uk2.srcs/sim_1/new/test.v
@@ -9,15 +9,19 @@ module test(
     reg signed[10:0]x;
     wire signed[15:0]y1;
     wire signed[15:0]y2;
+    wire signed[15:0]y3;
+    wire signed[15:0]y4;
     reg [10:0]data_sin[140000:0];
     reg [20:0]cnt;
     reg [15:0]v0=16'b101100001_0000000;//终端相对速度，9整7小
     reg [12:0]f0=13'b0110000000000;//载频，3整10小，单位GHz
-     integer file_out,file_out1;
+     integer file_out,file_out1,file_out2,file_out3;
     initial begin
     // $readmemb("C:/Users/jiang22/Desktop/fil2.txt",data_sin,0,140000); ///改动点数据矩阵长度  注意是"/"  而不是"\"
-     file_out = $fopen("D:/Users/tp2/res1.txt","w+");
-         file_out1 = $fopen("D:/Users/tp2/res2.txt","w+");
+     file_out = $fopen("D:/Users/tp2/res1.txt","w+");//内插后1
+         file_out1 = $fopen("D:/Users/tp2/res2.txt","w+");//内插后2
+         file_out2 = $fopen("D:/Users/tp2/res3.txt","w+"); //内插前1
+         file_out3 = $fopen("D:/Users/tp2/res4.txt","w+"); //内插前2
     clk=1;
     en=1;
     rst=0;
@@ -31,10 +35,12 @@ module test(
     cnt<=cnt+1;
      $fwrite(file_out, "%b\n",y1);
       $fwrite(file_out1, "%b\n",y2);
-    if(cnt==140000)
-   $finish;
+      $fwrite(file_out2, "%b\n",y3);
+      $fwrite(file_out3, "%b\n",y4);
+    //if(cnt==140000)
+   //$finish;
     x<=data_sin[cnt];
     end
     end
-    top test(clk,en,rst,tdl,v0,f0,y1,y2);
+    top test(clk,en,rst,tdl,v0,f0,y1,y2,y3,y4);
 endmodule 
--- top/uk2.srcs/sources_1/new/top.v
+++ top/uk2.srcs/sources_1/new/top.v
@@ -10,7 +10,9 @@ input [12:0]f0,//载频，3整10小，单位GHz
 //input signed[INPUT_WIDTH:0]xr,//输入同相和正交分量
 //input signed[INPUT_WIDTH:0]xi,
 output signed[15:0]yr,//输出同相和正交分量
-output signed[15:0]yi
+output signed[15:0]yi,
+output signed[15:0]y3,
+output signed[15:0]y4
     );
 reg [5:0]path;//路径数量
 wire [3:0]l;//内插指数
@@ -35,4 +37,6 @@ uk uk1(clk,v0,f0,en,rst,tdl,path,l,u[0][0],u[0][1],u[0][2],u[0][3],u[0][4],u[0][
 fifs f1(clk,en,~rst,l,u[0][0],u[0][1],u[0][2],u[0][3],u[0][4],u[0][5],u[0][6],u[0][7],u[0][8],u[0][9],u[0][10],u[0][11],u[0][12],u[0][13],u[0][14],u[0][15],u[0][16],u[0][17],u[0][18],u[0][19],u[0][20],u[0][21],u[0][22],u[0][23],u[1][0],u[1][1],u[1][2],u[1][3],u[1][4],u[1][5],u[1][6],u[1][7],u[1][8],u[1][9],u[1][10],u[1][11],u[1][12],u[1][13],u[1][14],u[1][15],u[1][16],u[1][17],u[1][18],u[1][19],u[1][20],u[1][21],u[1][22],u[1][23],rayleh[0][0],rayleh[0][1],rayleh[0][2],rayleh[0][3],rayleh[0][4],rayleh[0][5],rayleh[0][6],rayleh[0][7],rayleh[0][8],rayleh[0][9],rayleh[0][10],rayleh[0][11],rayleh[0][12],rayleh[0][13],rayleh[0][14],rayleh[0][15],rayleh[0][16],rayleh[0][17],rayleh[0][18],rayleh[0][19],rayleh[0][20],rayleh[0][21],rayleh[0][22],rayleh[0][23],rayleh[1][0],rayleh[1][1],rayleh[1][2],rayleh[1][3],rayleh[1][4],rayleh[1][5],rayleh[1][6],rayleh[1][7],rayleh[1][8],rayleh[1][9],rayleh[1][10],rayleh[1][11],rayleh[1][12],rayleh[1][13],rayleh[1][14],rayleh[1][15],rayleh[1][16],rayleh[1][17],rayleh[1][18],rayleh[1][19],rayleh[1][20],rayleh[1][21],rayleh[1][22],rayleh[1][23]);
 assign yr=rayleh[0][1];
 assign yi=rayleh[1][1];
+assign y3 = u[0][0];
+assign y4 = u[1][0];
 endmodule
